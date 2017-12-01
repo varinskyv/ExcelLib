0ed088e899cd0d5a97d5d969cb7a1daf4e729f83
varinskyv <varinskyv@gmail.com>
2017-12-01 10:20:40 +0500

    Выборка данных из Excel

---
+++ ExcelLib/Excel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using ExcelDataReader;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+
+namespace ExcelLib
+{
+    //Класс открывает документ и считывает содержимое
+    public class Excel: IDisposable
+    {
+        public delegate void CallBackHandler(bool result);
+        private CallBackHandler resultListener;
+
+        private Thread classThread;
+
+        private List<List<object>> sheets = new List<List<object>>();
+
+        /// <summary>
+        /// Содержит считанные страницы документа
+        /// </summary>
+        public List<List<object>> Sheets {
+            get
+            {
+                return sheets;
+            }
+        }
+
+        //Асинхронный метод, передающий результат в вызывающий класс
+        private bool CallBack(bool result)
+        {
+            return result;
+        }
+
+        /// <summary>
+        /// Получение данных из Excel файла
+        /// </summary>
+        /// <param name="fileName">имя файла</param>
+        /// <param name="resultListener">Функция обратного вызова</param>
+        public void GetData(string fileName, CallBackHandler resultListener)
+        {
+            this.resultListener = resultListener;
+
+            classThread = new Thread(new ParameterizedThreadStart(_getData));
+            classThread.Start(fileName);
+        }
+
+        // Метод получения данных, вызываемый в отдельном потоке
+        private void _getData(object fileName)
+        {
+            bool result = false;
+
+            try
+            {
+                var file = new FileInfo((string)fileName);
+                using (var stream = new FileStream((string)fileName, FileMode.Open, FileAccess.Read))
+                {
+                    IExcelDataReader reader = null;
+                    if (file.Extension == ".xls")
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    }
+                    else if (file.Extension == ".xlsx")
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+
+                    if (reader != null)
+                    {
+                        int sheet = 0;
+                        do
+                        {
+                            sheets.Add(new List<object>() { "Sheet" + sheet.ToString() });
+
+                            while (reader.Read())
+                            {
+                                object[] tmp = new object[reader.FieldCount];
+
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                    tmp[i] = reader.GetValue(i);
+
+                                tmp = tmp.Where(item => item != null).ToArray();
+
+                                if (tmp.Length > 0)
+                                {
+                                    sheets.Add(tmp.ToList());
+
+                                    if (!result)
+                                        result = true;
+                                }
+                            }
+
+                            sheet++;
+                        }
+                        while (reader.NextResult());
+
+                        reader.Dispose();
+                        reader = null;
+
+                        stream.Dispose();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            resultListener?.Invoke(result);
+        }
+
+        private Array ResizeArray(Array arr, int[] newSizes)
+        {
+            if (newSizes.Length != arr.Rank)
+                throw new ArgumentException("arr must have the same number of dimensions " +
+                                            "as there are elements in newSizes", "newSizes");
+
+            var temp = Array.CreateInstance(arr.GetType().GetElementType(), newSizes);
+            int length = arr.Length <= temp.Length ? arr.Length : temp.Length;
+            Array.ConstrainedCopy(arr, 0, temp, 0, length);
+
+            return temp;
+        }
+
+        //Деструктор
+        public void Dispose()
+        {
+            classThread.Abort();
+            classThread = null;
+
+            resultListener = null;
+
+            sheets = null;
+
+            GC.GetTotalMemory(true);
+        }
+    }
+}
---
+++ ExcelLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ExcelLib")]
+[assembly: AssemblyDescription("Библиотека для получения данных из Excel файла")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Varinsky Viktor. Innovatis")]
+[assembly: AssemblyProduct("ExcelLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(true)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bf3e6e7d-bd58-4486-bd78-9c33aee87fa4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
