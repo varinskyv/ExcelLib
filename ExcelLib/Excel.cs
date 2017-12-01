fa89b2b1530b0a9920646e47027f7687ea114537
varinskyv <varinskyv@gmail.com>
2017-12-01 10:42:52 +0500

    Удаление ненужного метода

--- ExcelLib/Excel.cs
+++ ExcelLib/Excel.cs
@@ -110,19 +110,6 @@ namespace ExcelLib
             resultListener?.Invoke(result);
         }
 
-        private Array ResizeArray(Array arr, int[] newSizes)
-        {
-            if (newSizes.Length != arr.Rank)
-                throw new ArgumentException("arr must have the same number of dimensions " +
-                                            "as there are elements in newSizes", "newSizes");
-
-            var temp = Array.CreateInstance(arr.GetType().GetElementType(), newSizes);
-            int length = arr.Length <= temp.Length ? arr.Length : temp.Length;
-            Array.ConstrainedCopy(arr, 0, temp, 0, length);
-
-            return temp;
-        }
-
         //Деструктор
         public void Dispose()
         {
